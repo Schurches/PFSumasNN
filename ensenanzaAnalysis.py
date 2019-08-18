dd7ff0dc338184e4a35b9bc494e0975300104eaf
Schurches <steveniglesiasm@gmail.com>
2019-08-17 23:35:09 -0500

    Documentación actualizada

--- ensenanzaAnalysis.py
+++ ensenanzaAnalysis.py
@@ -24,8 +24,11 @@ from sklearn.externals import joblib
 #Serialization
 import pickle;
 
-
-#rms = sqrt(mean_squared_error(Y_test,y_pred1))
+def isPublicOrPrivate(user):
+    if(user['escuela'] <= 2):
+        return 1
+    else:
+        return 0
 
 def isAnswerCorrect(df):
     df['respuesta'] = df['respuesta'].apply(str)
@@ -33,11 +36,20 @@ def isAnswerCorrect(df):
     isCorrect = (df['respuesta'] == df['respuestaJ']);
     return 1*isCorrect;
     
+def createDummyArrayFromSchools(schools):
+    return pd.get_dummies(schools)
+
 def convertSchool(user):
     if(user['escuela']=='Hartford International School'):
-        return 1
-    else:
         return 0
+    elif(user['escuela'] == 'I.E.D. Marie Poussepin'):
+        return 3
+    elif(user['escuela'] == 'I.E.D Marco Fidel Suárez'):
+        return 4
+    elif(user['escuela'] == 'Colegio del Sagrado Corazón'):
+        return 1
+    else: 
+        return 2
 
 def giveScoreToAnswer(answer, timesMedian):
     time = answer['tiempo'];
@@ -75,25 +87,25 @@ def generalGroupByTimeMEDIAN(data):
 def dataComparison(schools,data,row,col,figIndex,titulo):
     for i in range(1,6):
         plt.figure(figIndex)
-        plt.subplot(2,3,i)
+        ax = plt.subplot(2,3,i)
         for j in range(0,len(schools)):
             plt.plot(data[j].loc[lambda x: x.loID == i-1][row],data[j].loc[lambda x: x.loID == i-1][col], label=schools[j]['escuela'][1])
         
-        plt.xlabel = 'Grado'
+        ax.set_xlabel('Grado', fontsize=12)
         if(col=='isCorrect'):
             plt.ylim(0,1)
-            plt.ylabel = '% de correctas'
+            ax.set_ylabel('% de correctas', fontsize=13)
         elif(col=='answerChangedCount'):
             plt.plot([1,2,3,4,5],[1,1,1,1,1], label='Ideal')
-            plt.ylabel = 'Promedio de selección'
+            ax.set_ylabel('Promedio de selección', fontsize=13)
             plt.ylim(0,2)
         else:
-            plt.ylabel = 'Tiempo'
+            ax.set_ylabel('Tiempo', fontsize=13)
         if(i==5):
             plt.suptitle(titulo)
             plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        plt.title("Objetivo de aprendizaje #"+str(i))
-                  
+        plt.title("Objetivo de aprendizaje #"+str(i), fontsize=13)
+    #plt.savefig(titulo+'.png')
 
 def predictionsAnalysis(testValues, predictions, size):
     LO0 = []
@@ -110,7 +122,7 @@ def predictionsAnalysis(testValues, predictions, size):
     
     for i in range(1,6):
         plt.figure(5)
-        plt.subplot(2,3,i)
+        ax = plt.subplot(2,3,i)
         if(i == 1):
             testArray = testValues['LOIN0']
             LO = LO0;
@@ -129,13 +141,15 @@ def predictionsAnalysis(testValues, predictions, size):
             
         plt.scatter(list(range(0,size)),testArray)
         plt.scatter(list(range(0,size)),LO)
-        plt.plot(list(range(0,size)),testArray,label='Actual value')
-        plt.plot(list(range(0,size)),LO,label='Prediction')
-        plt.xlabel=('Observation')
-        plt.ylabel=('Value')
+        plt.plot(list(range(0,size)),testArray,label='Valor Real')
+        plt.plot(list(range(0,size)),LO,label='Predicción')
+        ax.set_xlabel('Observación',fontsize=13)
+        ax.set_ylabel('Intensidad',fontsize=13)
         plt.ylim(0,1)
+        plt.title('Objetivo de aprendizaje #'+str(i))
         if(i == 5):
             plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+            plt.suptitle('Comparación entre valor real y la predicción del modelo')
 
 
 def getIntensityDF(intensityDF):
@@ -145,8 +159,10 @@ def getIntensityDF(intensityDF):
     LO3Array = []
     LO4Array = []
     IDArray = []
-    for i in range(0, 1135, 5):
+    for i in range(0, len(scoresMeans), 5):
         total = intensityDF.loc[i].intensity+intensityDF.loc[i+1].intensity+intensityDF.loc[i+2].intensity+intensityDF.loc[i+3].intensity+intensityDF.loc[i+4].intensity        
+        if(total == 0):
+            total = 1
         IDArray.append(intensityDF.loc[i].userID)
         LO0Array.append(intensityDF.loc[i].intensity/total)
         LO1Array.append(intensityDF.loc[i+1].intensity/total)
@@ -158,29 +174,83 @@ def getIntensityDF(intensityDF):
 
 
 ############################
-### Reading the datasets ###
-hartfortDF = pd.read_csv('datasets/firstModel/datos_hartfort.csv')
-hartfortDF = hartfortDF.drop(labels=['Unnamed: 0'],axis=1)
+### Reading the datasets ###    
+
+hartfortDF = pd.read_csv('./datasets/firstModel/datos_hartfort.csv');
+hartfortDF = hartfortDF.drop(labels=['Unnamed: 0'],axis=1);
 ensenanzaDF = pd.read_csv('datasets/firstModel/datos_la_ensenanza.csv');
 ensenanzaDF = ensenanzaDF.drop(labels=['Unnamed: 0'],axis=1)
-#hartfortDF = hartfortDF.loc[lambda x: x.genero == 1]
+marcoDF = pd.read_csv('datasets/firstModel/datos_marco_fidel.csv');
+marcoDF = marcoDF.drop(labels=['Unnamed: 0'],axis=1)
+sagradoDF = pd.read_csv('datasets/firstModel/datos_sagrado.csv');
+sagradoDF = sagradoDF.drop(labels=['Unnamed: 0'],axis=1)
+marieDF = pd.read_csv('datasets/firstModel/datos_marie_poussepin.csv');
+marieDF = marieDF.drop(labels=['Unnamed: 0'],axis=1)
+
 schoolsDF = []
+publicSchools = []
+privateSchools = []
 schoolsDF.append(hartfortDF)
 schoolsDF.append(ensenanzaDF)
+schoolsDF.append(marcoDF)
+schoolsDF.append(sagradoDF)
+schoolsDF.append(marieDF)
+
+publicSchools.append(marcoDF)
+publicSchools.append(marieDF)
+privateSchools.append(hartfortDF)
+privateSchools.append(ensenanzaDF)
+privateSchools.append(sagradoDF)
+
 
 ###########################
 ####### Analysis ##########
 schoolsMeansGB = []
 schoolsMediansGB = []
 schoolsGeneralGroupBy = []
+
 schoolsMeansGB.append(generalGroupByTimeMEAN(hartfortDF).reset_index())
 schoolsMeansGB.append(generalGroupByTimeMEAN(ensenanzaDF).reset_index())
+schoolsMeansGB.append(generalGroupByTimeMEAN(marcoDF).reset_index())
+schoolsMeansGB.append(generalGroupByTimeMEAN(sagradoDF).reset_index())
+schoolsMeansGB.append(generalGroupByTimeMEAN(marieDF).reset_index())
+
 schoolsMediansGB.append(generalGroupByTimeMEDIAN(hartfortDF).reset_index())
 schoolsMediansGB.append(generalGroupByTimeMEDIAN(ensenanzaDF).reset_index())
+schoolsMediansGB.append(generalGroupByTimeMEDIAN(marcoDF).reset_index())
+schoolsMediansGB.append(generalGroupByTimeMEDIAN(sagradoDF).reset_index())
+schoolsMediansGB.append(generalGroupByTimeMEDIAN(marieDF).reset_index())
+
 schoolsGeneralGroupBy.append(generalGroupBy(hartfortDF).reset_index())
 schoolsGeneralGroupBy.append(generalGroupBy(ensenanzaDF).reset_index())
+schoolsGeneralGroupBy.append(generalGroupBy(marcoDF).reset_index())
+schoolsGeneralGroupBy.append(generalGroupBy(sagradoDF).reset_index())
+schoolsGeneralGroupBy.append(generalGroupBy(marieDF).reset_index())
+
+publicMeans = []
+privateMeans = []
+publicGeneral = []
+privateGeneral = []
+publicMeans.append(schoolsMeansGB[2])
+publicMeans.append(schoolsMeansGB[4])
+privateMeans.append(schoolsMeansGB[0])
+privateMeans.append(schoolsMeansGB[1])
+privateMeans.append(schoolsMeansGB[3])
+
+publicGeneral.append(schoolsGeneralGroupBy[2])
+publicGeneral.append(schoolsGeneralGroupBy[4])
+privateGeneral.append(schoolsGeneralGroupBy[0])
+privateGeneral.append(schoolsGeneralGroupBy[1])
+privateGeneral.append(schoolsGeneralGroupBy[3])
+
+dataComparison(publicSchools, publicMeans,   'grado','tiempo',   24, 'Promedio de tiempos por colegio público')
+dataComparison(publicSchools, publicGeneral, 'grado','isCorrect',25, 'Porcentaje de respuestas correctas por colegio público')
+dataComparison(privateSchools,privateMeans,  'grado','tiempo',   26, 'Promedio de tiempos por colegio privado')
+dataComparison(privateSchools,privateGeneral,'grado','isCorrect',27, 'Porcentaje de respuestas correctas por colegio privado')
+
+
 #Time analysis MEAN
-dataComparison(schoolsDF,schoolsMeansGB, 'grado', 'tiempo', 1,'Promedio de tiempos por colegio')
+dataComparison(schoolsDF,schoolsMeansGB, 'grado', 'tiempo', 1,'Promedio de tiempos por colegio ')
 #Time analysis MEDIAN (most accurate)
 dataComparison(schoolsDF,schoolsMediansGB, 'grado', 'tiempo', 2, 'Mediana de tiempos por colegio')
 #is Correct % analysis
@@ -191,14 +261,20 @@ dataComparison(schoolsDF,schoolsGeneralGroupBy, 'grado', 'isCorrect', 3, 'Pregun
 #Adding score to each school
 hartfortDF['score'] = hartfortDF.apply(lambda x: giveScoreToAnswer(x, generalGroupByTimeMEDIAN(hartfortDF)), axis=1)
 ensenanzaDF['score'] = ensenanzaDF.apply(lambda x: giveScoreToAnswer(x, generalGroupByTimeMEDIAN(ensenanzaDF)), axis=1)
+marcoDF['score'] = marcoDF.apply(lambda x: giveScoreToAnswer(x, generalGroupByTimeMEDIAN(marcoDF)), axis=1)
+sagradoDF['score'] = sagradoDF.apply(lambda x: giveScoreToAnswer(x, generalGroupByTimeMEDIAN(sagradoDF)), axis=1)
+marieDF['score'] = marieDF.apply(lambda x: giveScoreToAnswer(x, generalGroupByTimeMEDIAN(marieDF)), axis=1)
 
-#sns.distplot(hartfortDF['tiempo'])
+#sns.distplot(sagradoDF['tiempo'])
 #sns.distplot(ensenanzaDF['tiempo'])
 
 #Merging datasets
 ensenanzaDF = ensenanzaDF.drop(labels=['answerChangedCount','problema'],axis=1)
+marcoDF = marcoDF.drop(labels=['answerChangedCount','problema'],axis=1)
+sagradoDF = sagradoDF.drop(labels=['answerChangedCount','problema'],axis=1)
+marieDF = marieDF.drop(labels=['answerChangedCount','problema'],axis=1)
 hartfortDF = hartfortDF.drop(labels=['problema'],axis=1)
-mergedDF = pd.concat([hartfortDF, ensenanzaDF]).reset_index(drop=True)
+mergedDF = pd.concat([hartfortDF, ensenanzaDF, marcoDF, sagradoDF, marieDF]).reset_index(drop=True)
 
 #Grouping users
 scoresMeans = mergedDF.groupby(['userID','loID'])['score'].mean().reset_index();
@@ -207,6 +283,11 @@ scoresMeans = scoresMeans.round({'intensity':1})
 
 #General user information
 mergedDF['escuela'] = mergedDF.apply(convertSchool,axis=1)
+mergedDF['escuela'] = mergedDF.apply(isPublicOrPrivate,axis=1)
+
+#mergedDF[['school1','school2','school3','school4', 'school5']] = escuelas
+#mergedDF = mergedDF.drop(labels=['escuela','school5'],axis=1)
+
 generalUserInfo = mergedDF.groupby(['userID']).mean()
 generalUserInfo = generalUserInfo.drop(labels=['loID','tiempo','isCorrect','score'],axis=1);
 
@@ -228,7 +309,7 @@ sc = StandardScaler() #Must be scaled to avoid variable domination
 #pickle.dumps(X.describe(),'normalizingData')
 #X = preprocessing.normalize(X,axis=1)
 trainedScaler = sc.fit(X_train);
-joblib.dump(sc, 'scaler.joblib');
+#joblib.dump(sc, 'scaler.joblib');
 X_train = trainedScaler.transform(X_train)
 X_test = trainedScaler.transform(X_test)
 
@@ -242,8 +323,7 @@ modelHistory = classifier.fit(X_train, y_train, epochs=50)
 y_pred = classifier.predict(X_test);
 
 predictionsAnalysis(y_test, y_pred, len(y_pred))
-
-classifier.save('NewPlayerNeuralNet.h5')
+#classifier.save('NewPlayerNeuralNet.h5')
 
 
 
